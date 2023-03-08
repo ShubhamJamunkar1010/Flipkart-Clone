ec14f797c5a7d7f3bd15bf090c076606204361fa
shubham jamunkar <shubhamjamunkar4@gmail.com>
2023-03-08 13:08:51 +0530

    go

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,15 +2,13 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { addCart, delCart } from "../redux/action";
-import './Cart.css';
+import "./Cart.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Cart = () => {
   const state = useSelector((state) => state.handleCart);
   const dispatch = useDispatch();
-  const { loginWithRedirect, isAuthenticated} = useAuth0();
-  let GST=0;
-  
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   const handleAdd = (item) => {
     dispatch(addCart(item));
@@ -19,70 +17,60 @@ const Cart = () => {
     dispatch(delCart(item));
   };
   const emptyCart = () => {
-    return (
-            <h3 className="emptyCart">Your Cart is Empty</h3>
-    );
+    return <h3 className="emptyCart">Your Cart is Empty</h3>;
   };
-  
+
   const cartItems = (product) => {
     return (
       <div>
-      <div className="cart-products">
-      <div className="p-image">
-      <img
-                  src={product.image}
-                  alt={product.title}
-                  height="200px"
-                  width="180px"
-                />
+        <div className="cart-products">
+          <div className="p-image">
+            <img
+              src={product.image}
+              alt={product.title}
+              height="200px"
+              width="180px"
+            />
+          </div>
+          <div className="p-details">
+            <h3 className="p-title">{product.title}</h3>
+            <p className="p-price">
+              {product.qty} X ${product.price} = ${product.qty * product.price}
+              <p>GST = {(product.qty * product.price) / 15} </p>
+              <p>
+                Total = $
+                {(
+                  product.qty * product.price +
+                  (product.qty * product.price) / 15
+                ).toFixed(2)}{" "}
+              </p>
+            </p>
+            <button className="p-btn1" onClick={() => handleDel(product)}>
+              <i className="fa fa-minus"></i>
+            </button>
+            <button className="p-btn2" onClick={() => handleAdd(product)}>
+              <i className="fa fa-plus"></i>
+            </button>
+          </div>
         </div>
-        <div className="p-details">
-                <h3 className="p-title">{product.title}</h3>
-                <p className="p-price">
-                  {product.qty} X ${product.price} = ${(product.qty * product.price)}
-                  <p>GST = {(product.qty * product.price)/15} </p>
-                  <p>Total = ${((product.qty * product.price) + (product.qty * product.price)/15).toFixed(2)} </p>
-                </p>
-                <button
-                className="p-btn1"
-                  onClick={() => handleDel(product)}
-                >
-                  <i className="fa fa-minus"></i>
-                </button>
-                <button
-                className="p-btn2"
-                  onClick={() => handleAdd(product)}
-                >
-                  <i className="fa fa-plus"></i>
-                </button>  
       </div>
-      </div> 
-      </div>
-      
     );
   };
-  
+
   const buttons = () => {
     return (
       <>
-      <div className="C-btn">
-        {isAuthenticated ?(
-         <NavLink
-         to="/checkout"
-         className="checkout-btn"
-       >
-         Proceed to Checkout
-       </NavLink>
-        ):(
-            <div className="checkout-btn"
-            onClick={() => loginWithRedirect()}
-              >
+        <div className="C-btn">
+          {isAuthenticated ? (
+            <NavLink to="/checkout" className="checkout-btn">
+              Proceed to Checkout
+            </NavLink>
+          ) : (
+            <div className="checkout-btn" onClick={() => loginWithRedirect()}>
               Proceed to Checkout
             </div>
-        )
-       }
-           
-      </div>
+          )}
+        </div>
       </>
     );
   };
@@ -98,127 +86,3 @@ const Cart = () => {
 
 export default Cart;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useRef } from "react";
-// import Home from "./Home";
-// import "./Login.css";
-
-// function Login() {
-//   const email = useRef();
-//   const password = useRef();
-//   const getEmail = localStorage.getItem("emailData");
-//   const getPassword = localStorage.getItem("passwordData");
-//   const handleSubmit = () => {
-//     if (email.current.value == "shubham" && password.current.value == "1027") {
-//       localStorage.setItem("emailData", "shubham");
-//       localStorage.setItem("passwordData", "1027");
-//     }
-//   };
-
-//   return (
-//     <div className="loginPage">
-//       {getEmail && getPassword ? (
-//         <Home />
-//       ) : (
-//         <form className="form" onSubmit={handleSubmit}>
-//           <div>
-//             <h1 style={{ color: "white" }}>Login to Flipcart</h1>
-//           </div>
-//           <div>
-//             <input
-//               className="email"
-//               type="text"
-//               placeholder="enter your email"
-//               ref={email}
-//             />
-//           </div>
-//           <div>
-//             <input
-//               className="pass"
-//               type="password"
-//               placeholder="enter your password"
-//               ref={password}
-//             />
-//           </div>
-
-//           <button className="log-btn">Login</button>
-//         </form>
-//       )}
-//     </div>
-//   );
-// }
-// export default Login;
-
-
-
-
-
-
-// Login.className.loginPage{
-//     display: flex;
-//     flex-direction: column;
-//     height: 100vh;
-//     background-color: rgb(48, 48, 121);
-// }
-// .form{
-//     display: flex;
-//     flex-direction: column;
-//     gap: 5vh;
-//     align-items: center;
-//     justify-content: center;
-//     margin-top: 20vh;
-// }
-// .log-btn{
-//     background-color: blue;
-//     color: rgb(223, 215, 215);
-//     width: 300px;
-//     padding: 10px;
-//     border: 1px solid blue;
-//     font-weight: bold;
-// }
-// .log-btn:hover{
-//     background-color: rgb(66, 66, 234);
-// }
-// .email{
-//     width: 300px;
-//     padding: 10px;
-// }
-// .pass{
-//     width: 300px;
-//     padding: 10px;
-// }
